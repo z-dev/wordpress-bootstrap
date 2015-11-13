457c09704ae6763b4d1d56c01bd98971d54970d8
Richard Gill <richard@rgill.co.uk>
2015-11-13 18:51:24 +0000

    Got docker running

--- src/index.php
+++ src/index.php
@@ -1,34 +1,34 @@
 <?php get_header(); ?>
-			
+
 			<?php
 				$blog_hero = of_get_option('blog_hero');
 				if ($blog_hero){
 			?>
 			<div class="clearfix row-fluid">
 				<div class="hero-unit">
-				
+
 					<h1><?php bloginfo('title'); ?></h1>
-					
+
 					<p><?php bloginfo('description'); ?></p>
-				
+
 				</div>
 			</div>
 			<?php
 				}
 			?>
-			
+
 			<div id="content" class="clearfix row-fluid">
-			
+
 				<div id="main" class="span8 clearfix" role="main">
 
 					<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-						<?php get_template_part( 'content', get_post_format() ); ?>					
-					<?php endwhile; ?>	
-					
+						<?php get_template_part( 'content', get_post_format() ); ?>
+					<?php endwhile; ?>
+
 					<?php if (function_exists('page_navi')) { // if expirimental feature is active ?>
-						
+
 						<?php page_navi(); // use the page navi function ?>
-						
+
 					<?php } else { // if it is disabled, display regular wp prev & next links ?>
 						<nav class="wp-prev-next">
 							<ul class="clearfix">
@@ -36,10 +36,10 @@
 								<li class="next-link"><?php previous_posts_link(_e('Newer Entries &raquo;', "bonestheme")) ?></li>
 							</ul>
 						</nav>
-					<?php } ?>		
-					
+					<?php } ?>
+
 					<?php else : ?>
-					
+
 					<article id="post-not-found">
 					    <header>
 					    	<h1><?php _e("Not Found", "bonestheme"); ?></h1>
@@ -50,13 +50,13 @@
 					    <footer>
 					    </footer>
 					</article>
-					
+
 					<?php endif; ?>
-			
+
 				</div> <!-- end #main -->
-    
+
 				<?php get_sidebar(); // sidebar 1 ?>
-    
+
 			</div> <!-- end #content -->
 
 <?php get_footer(); ?>
